6ae300fbfecb7f766961014f840308980a8f038a
Gabriel Lima <baiagabriel1@gmail.com>
2022-04-06 14:39:50 -0300

    Ex - 03 - Busca Endereço Parte 1 (adição de await e alteração para arrow function)

--- ex_03/script.mjs
+++ ex_03/script.mjs
@@ -1,7 +1,12 @@
-function buscar() {
+const fetchApi = async () => {
     let elementCep = document.getElementById('numberCep')
     let nCep = Number(elementCep.value);
-    requisicaoCep = fetch(`https://viacep.com.br/ws/${nCep}/json/`)
-    console.log(requisicaoCep)
-
-}
+    
+    let response = await fetch(`https://viacep.com.br/ws/${nCep}/json/`)
+    if (!response.ok) {
+        const err = await response.json()
+        return console.log('Ocorreu o seguinte erro:', err.message);
+    }
+    const data = await response.json();
+    console.log(data);
+};
